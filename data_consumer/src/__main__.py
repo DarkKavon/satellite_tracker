1284f581466a3157b41408ab7fc81ed27a07d2e3
DarkKavon <kacperwandel@gmail.com>
2023-08-06 11:15:12 +0200

    add data consumer

--- data_consumer/src/__main__.py
+++ data_consumer/src/__main__.py
@@ -1,10 +1,17 @@
 import sys
 sys.path.append('.')
+import json
+import logging
 from kafka import KafkaConsumer
 from data_consumer.src.connectors.mongo_connector import MongoDBConnector
 
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)s %(message)s'
+    )
+    logger = logging.getLogger()
 
     TOPIC = "SATELLITE_TRACKER"
     mongo_url = "mongodb://localhost:27017"
@@ -12,8 +19,11 @@ if __name__ == "__main__":
     mongo_collection = "satellites"
 
     consumer = KafkaConsumer(TOPIC, bootstrap_servers = "localhost:29092")
+    logger.info("Kafka consumer registered...")
     mdb = MongoDBConnector(mongo_url, mongo_db)
+    logger.info("Connection with MongoDB established...")
 
+    logger.info("Start reading records...")
     for record in consumer:
-        print(record)
-
+        mdb.insert(mongo_collection, json.loads(record.value.decode()))
+        logger.info(f"Record about {json.loads(record.value.decode())['satellite_name']} inserted to database...")
